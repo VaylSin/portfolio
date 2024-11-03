ca22bc3a604cfb6925459fb648851222aef7f546
SlyB33 <skvmOmega@gmail.com>
2024-11-03 23:03:24 +0100

    param autoload, sass file , preset - mixins

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,36 @@
-// webpack.config.js
-
 const Encore = require('@symfony/webpack-encore');
 
 Encore
-    // directory where compiled assets will be stored
     .setOutputPath('public/build/')
-    // public path used by the web server to access the output path
     .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
-
-    /*
-     * ENTRY CONFIG
-     *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-     */
     .addEntry('app', './assets/app.js')
-
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
-
-    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    // .enableStimulusBridge('./assets/controllers.json')
     .splitEntryChunks()
-
-    // will require an extra script tag for runtime.js
-    // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
-
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
     .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
+    // .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
-
     .configureBabel((config) => {
         config.plugins.push('@babel/plugin-proposal-class-properties');
     })
-
-    // enables @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
         config.corejs = 3;
     })
-
     .enableSassLoader()
     .enablePostCssLoader()
+    .configureDevServerOptions(options => {
+        options.liveReload = true;
+        options.hot = true;
+        options.watchFiles = ['assets/**/*'];
+        options.client = {
+            overlay: {
+                warnings: false,
+                errors: true,
+            },
+        };
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
